33ab84229280bac6a75fd8951e1fdd865a63fedf
kaustav Mani Deka <chwapnamkashyap@gmail.com>
2025-12-06 02:00:31 +0530

    Adjust height of figure element to 101vh

--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
       <ParallaxBackground />
       <figure
         className="absolute inset-0"
-        style={{ width: "100vw", height: "100vh" }}
+        style={{ width: "100vw", height: "101vh" }}
       >
         <Canvas camera={{ position: [0, 1, 3] }}>
           <Suspense fallback={<Loader />}>
